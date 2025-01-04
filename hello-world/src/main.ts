05de51787318b1b678545f78b508841f317c5683
TheS1lentArr0w <paulocruz0123@hotmail.com>
2025-01-04 00:29:19 +0000

    lesson: conditional branching - if and ?

--- hello-world/src/main.ts
+++ hello-world/src/main.ts
@@ -1,4 +1,16 @@
-let name = prompt("What's your name?", "Name");
+let message;
 
-alert(name);
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
 
+let message = (login == 'Employee') ? 'Hello' :
+	(login == 'Director') ? 'Greetings' :
+	(login == '') ? 'No login' :
+	'';
