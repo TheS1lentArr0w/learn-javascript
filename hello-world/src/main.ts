4ab671e2e5eabed3feb07fe57898cd06bcd04a17
TheS1lentArr0w <paulocruz0123@hotmail.com>
2025-01-06 19:08:23 +0000

    lesson: completed switch statement

--- hello-world/src/main.ts
+++ hello-world/src/main.ts
@@ -1,5 +1,42 @@
-let age = Number(prompt("What is your age?", "Age"));
+// switch (browser) {
+//   case 'Edge':
+//     alert( "You've got the Edge!" );
+//     break;
+// 
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert( 'Okay we support these browsers too' );
+//     break;
+// 
+//   default:
+//     alert( 'We hope that this page looks ok!' );
+// }
 
-if (age >= 14 && age <= 90) {
-    alert("Age is in the range");
+// let a = +prompt('a?', '');
+// 
+// if (a == 0) {
+//   alert( 0 );
+// }
+// if (a == 1) {
+//   alert( 1 );
+// }
+// 
+// if (a == 2 || a == 3) {
+//   alert( '2,3' );
+// }
+
+let a = Number(prompt('a?', ''));
+
+switch (a) {
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert('2,3');
 }
